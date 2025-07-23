ba7df3074db01e7aa3fd8d5c119a35d016a037d7
Jonathon Tech <jgt.tech@protonmail.com>
2025-07-22 22:20:00 -0500

    WIP

--- cmds/import/create.go
+++ cmds/import/create.go
@@ -1,12 +1,9 @@
 package _import
 
 import (
-	// "fmt"
 	"os"
-	// "path/filepath"
 
 	"github.com/jgttech/repo/src/env"
-	// "github.com/jgttech/repo/src/exec"
 	"github.com/jgttech/repo/src/fs/cp"
 )
 
@@ -21,23 +18,6 @@ func create(base string) (err error) {
 	if err = cp.Dir(cp.From(base), cp.To(env.BUILD_DIR)); err == nil {
 		err = os.RemoveAll(base)
 	}
-
-	// TODO
-	// Need to load the state and cli configs from the extracted archive.
-
-	// TODO
-	// Then need to iterate through the backups in the state config and
-	// re-create the backup files. Consider generating new backup files
-	// with updated timestamps.
-
-	// TODO
-	// Ensure the backup files that are re-created delete the originals
-	// so the the symlink works.
-
-	// dir := filepath.Join(env.BUILD_DIR, "..")
-	// arg := fmt.Sprintf("stow -t %s %s", env.HOME, env.CONST_DIR)
-	// cmd := exec.Cmd(arg, exec.Dir(dir), exec.Stdio)
-	// err = cmd.Run()
 
 	return
 }
--- cmds/import/main.go
+++ cmds/import/main.go
@@ -2,12 +2,16 @@ package _import
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/jgttech/repo/src/archive"
 	"github.com/jgttech/repo/src/env"
 	"github.com/jgttech/repo/src/errors"
+	"github.com/jgttech/repo/src/exec"
+	"github.com/jgttech/repo/src/state"
 	v3 "github.com/urfave/cli/v3"
 )
 
@@ -49,6 +53,31 @@ func Command() *v3.Command {
 			} else {
 				err = swap(base)
 			}
+
+			// TODO
+			// Need to load the state and cli configs from the extracted archive.
+
+			// TODO
+			// Then need to iterate through the backups in the state config and
+			// re-create the backup files. Consider generating new backup files
+			// with updated timestamps.
+
+			// TODO
+			// Ensure the backup files that are re-created delete the originals
+			// so the the symlink works.
+
+			// fmt.Println("BUILD_CLI...:", env.BUILD_CLI)
+			// fmt.Println("BUILD_STATE.:", env.BUILD_STATE)
+			// fmt.Println("BUILD_CONF..:", env.BUILD_CONF)
+
+			// dir := filepath.Join(env.BUILD_DIR, "..")
+			// arg := fmt.Sprintf("stow -t %s %s", env.HOME, env.CONST_DIR)
+			// cmd := exec.Cmd(arg, exec.Dir(dir), exec.Stdio)
+			// err = cmd.Run()
+
+			// WIP
+			// conf := &state.Conf{}
+			// conf.Load()
 
 			return
 		},
